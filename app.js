86b406cf85b064de191b9de3a66f64c4ef370aea
gloryarz <gloryarz@gmail.com>
2018-09-09 14:58:27 -0500

    Comenzado a escribir la respuesta a options

--- app.js
+++ app.js
@@ -4,6 +4,83 @@ const path = require('path');
 
 const filePath = './links.md';
 
+
+const absolutePath = path.resolve(filePath);
+
+const readMDFile = () => {
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      getData(data);
+      console.log('readMDFile');
+    }
+  });
+};
+
+let arr = [];
+const getData = (data) => {
+  let file = data;
+  let search = /\[.*https?:.*\)/ig;
+  let searching = file.match(search);
+
+  searching.forEach(hrefAndText => {
+    hrefAndText = hrefAndText.replace(/.$/, '');
+    hrefAndText = hrefAndText.replace(/^\[/, '');
+    hrefAndText = hrefAndText.split(/\]\(/);
+    arr.push(hrefAndText);
+  });
+  console.log('getData');
+  fetching();
+};
+
+const fetching = () => {
+  arr.forEach(element => {
+    let url = element[1];
+    fetch(url)
+      .then(res => fetchResult(res.url, res.status, res.statusText))
+      .then(console.log('fetch'))
+      .catch(err => console.log(err));
+  });
+};
+
+let fetchStatus = [];
+const fetchResult = (url, status, statusText) => {
+  fetchStatus.push([url, status, statusText]);
+  console.log('fetchResult');
+};
+fetchResult();
+
+const options = () => {
+ // readMDFile();
+  console.log(absolutePath, 'how', arr);
+  /*setTimeout((el) => {
+    console.log(absolutePath, arr);
+  }, 1000); */
+};
+
+let userValue = 'ahhh';
+
+const userInput = () => {
+  if (userValue === 'better') {
+    console.log('better');
+  } else {
+    options();
+  }
+};
+userInput();
+
+
+module.exports = {
+  readMDFile, 
+  getData,
+  fetching,
+  fetchResult,
+  options
+
+};
+
+/*
 const readMDFile = (errCallBack, callBack) => {
   fs.readFile(filePath, 'utf8', (err, data) => {
     if (err) {
@@ -42,64 +119,6 @@ const fetching = () => {
 let fetchStatus = [];
 const fetchResult = (url, status, statusText) => {
   fetchStatus.push([url, status, statusText]);
-  console.log(fetchStatus);
 };
 
-
-module.exports = {
-  readMDFile,
-  fetching
-};
-
-/*
-read( callBack = (data) => {
-    let file = data;
-    let search = /\[.*https?:.*\)/g;
-    let searching = file.match(search);
-    // console.log(searching) 
-    
-    searching.forEach(element => {
-                let name = /\[.*\]/g;
-        let urls = /https?:.*\)/g;
-        let getName = element.match(name);
-        let geturls = element.match(urls)
-        console.log(geturls, getName)
-    });
-});
-*/
-
-
-/* 
-const read = (errCallBack, callBack) => {
-    fs.readFile('./links.md', 'utf8', (err, data) => {
-        if (err) {
-            errCallBack(err)
-        } else {
-            callBack(data)
-        }
-    })
-};
-
-let arr = []
-read( (err) => console.log(err), callBack = (data) => {
-    let file = data;
-    let search = /\[.*https?:.*\)/ig;
-    let searching = file.match(search);
-
-    searching.forEach(element => {
-        element = element.replace(/.$/, '');
-        element = element.replace(/^\[/, '');
-        element = element.split(/\]\(/);
-        arr.push(element)
-    });
-    fetching();
-});
-
-const fetching  = () => {
-    arr.forEach(element => {
-        let url = element[1];
-        fetch(url)
-        .then(res => console.log(res.url, res.status, res.statusText))
-    });
-}
 */
